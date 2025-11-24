1540bf1a90c2f5a72302ea396702553f4dcd4f4c
darshaka <darshakasri@example.com>
2025-11-24 20:05:43 +0530

    Reserve now btn added

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Custom Product Theme',
+    'version': '0.0.1.0.1',
+    'license': 'LGPL-3',
+    'category': 'Theme',
+    'description': """Custom Theme for Products""",
+    'author': 'darshaka',
+    'depends': ['website',  'website_sale'],
+    'data': ['views/templates.xml'],
+    'sequence': 10,
+    'application': False,
+    'auto_install': False,
+}
