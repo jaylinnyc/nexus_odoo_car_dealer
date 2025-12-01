64d51b7d876b3a06771188d80dc94590f7b89c3d
darshaka <darshakasri@gmail.com>
2025-12-01 21:55:10 +0530

    test 33

--- models/calendar_event.py
+++ models/calendar_event.py
@@ -5,29 +5,27 @@ class CalendarEvent(models.Model):
 
     physical_product_id = fields.Many2one('product.product', string="Reserved Vehicle")
     
-    sale_order_id = fields.Many2one(
-        'sale.order',
-        string='Reservation Order',
-        compute='_compute_sale_order_id',
-        store=True,
-        readonly=True
-    )
+    # sale_order_id = fields.Many2one(
+    #     'sale.order',
+    #     string='Reservation Order',
+    #     compute='_compute_sale_order_id',
+    #     store=True,
+    #     readonly=True
+    # )
 
-    def _compute_sale_order_id(self):
-        for event in self:
-            event.sale_order_id = event.sale_order_line_ids and event.sale_order_line_ids[0].order_id or False
+    # def _compute_sale_order_id(self):
+    #     for event in self:
+    #         event.sale_order_id = event.sale_order_line_ids and event.sale_order_line_ids[0].order_id or False
             
-    def action_confirm_reservation_and_reduce_stock(self):
-        """
-        Confirms the reservation and reduces the vehicle from stock.
-        """
-        self.ensure_one()
-        product = self.physical_product_id
+    # def action_confirm_reservation_and_reduce_stock(self):
+    #     """
+    #     Confirms the reservation and reduces the vehicle from stock.
+    #     """
+    #     self.ensure_one()
+    #     product = self.physical_product_id
+
+    #     if not product:
+    #         return
         
-        # 1. Validation (Clean Code)
-        if not product:
-            # Not a vehicle reservation or product not set.
-            return
-        
-        product.product_tmpl_id.sudo().write({'website_published': False})
-        self.message_post(body=f"Vehicle **{product.display_name}** successfully reserved and **unpublished** from the website.")
+    #     product.product_tmpl_id.sudo().write({'website_published': False})
+    #     self.message_post(body=f"Vehicle **{product.display_name}** successfully reserved and **unpublished** from the website.")
