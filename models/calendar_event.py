f31b9ae767035d72a9bdc0efe1293dddf64ab926
darshaka <darshakasri@gmail.com>
2025-12-02 17:15:45 +0530

    find sale_order via search

--- models/calendar_event.py
+++ models/calendar_event.py
@@ -20,7 +20,9 @@ class CalendarEvent(models.Model):
     def _compute_sale_order_id(self):
         for event in self:
             _logger.info("Computing sale_order_id for Calendar Event ID %s", event.sale_order_line_ids)
-            event.sale_order_id = event.sale_order_line_ids and event.sale_order_line_ids[0].order_id or False
+            # event.sale_order_id = event.sale_order_line_ids and event.sale_order_line_ids[0].order_id or False
+            sale_order_line = self.env['sale.order.line'].search([('id', '=', event.sale_order_line_ids[0])])
+            event.sale_order_id = sale_order_line.order_id or False
             _logger.info("Sale order lines linked to this event: %s", event.sale_order_id)
             
     def action_confirm_reservation_and_unpublish_product(self):
