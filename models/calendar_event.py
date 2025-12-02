94278e396d381fe4867e7c1a2925f1ee1db0a972
darshaka <darshakasri@gmail.com>
2025-12-02 17:20:52 +0530

    test 35

--- models/calendar_event.py
+++ models/calendar_event.py
@@ -21,8 +21,8 @@ class CalendarEvent(models.Model):
         for event in self:
             _logger.info("Computing sale_order_id for Calendar Event ID %s", event.sale_order_line_ids)
             # event.sale_order_id = event.sale_order_line_ids and event.sale_order_line_ids[0].order_id or False
-            sale_order_line = self.env['sale.order.line'].search([('id', '=', event.sale_order_line_ids[0])])
-            event.sale_order_id = sale_order_line.order_id or False
+            sale_order_line = event.sale_order_line_ids[:1]
+            event.sale_order_id = sale_order_line.order_id if sale_order_line else False
             _logger.info("Sale order lines linked to this event: %s", event.sale_order_id)
             
     def action_confirm_reservation_and_unpublish_product(self):
