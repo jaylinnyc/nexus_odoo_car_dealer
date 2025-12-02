e1128ec2fa8471ecc7b5df81d03498274a640824
darshaka <darshakasri@gmail.com>
2025-12-02 22:51:11 +0530

    show vehicle data on info page

--- controller/web_appointment.py
+++ controller/web_appointment.py
@@ -9,22 +9,34 @@ _logger = logging.getLogger(__name__)
 
 # 2. UPDATE CLASS: Inherit from AppointmentController
 class WebsiteAppointmentExtended(AppointmentController):
-    
-    # Check if this route still matches the Odoo 19 standard, 
-    # or if you are defining a custom endpoint.
+
     @http.route(['/appointment/<int:appointment_type_id>'], type='http', auth="public", website=True, csrf=True)
     def appointment_submit(self, appointment_type_id=None, **kwargs):
         response = super(WebsiteAppointmentExtended, self).appointment_type_page(appointment_type_id)
         
-        # Parse params from URL
         product_id = kwargs.get('product_id')
-        _logger.info("Received appointment submission with product_id: %s", product_id)
-        product = request.env['product.product'].sudo().browse(int(product_id))
+        product_name = kwargs.get('product_name')
+        product_price = kwargs.get('product_price')
         
         if response.qcontext:
             response.qcontext['product_id'] = product_id
-            response.qcontext['product_name'] = product.name
-            response.qcontext['product_price'] = f"${product.lst_price:.2f}" 
+            response.qcontext['product_name'] = product_name
+            response.qcontext['product_price'] = product_price 
+        
+        return response
+    
+    @http.route(['/appointment/<int:appointment_type_id>/info'], type='http', auth="public", website=True, csrf=True)
+    def appointment_info(self, appointment_type_id=None, **kwargs):
+        response = super(WebsiteAppointmentExtended, self).appointment_type_id_form(appointment_type_id, **kwargs)
+        
+        product_id = kwargs.get('product_id')
+        product_name = kwargs.get('product_name')
+        product_price = kwargs.get('product_price')
+        
+        if response.qcontext:
+            response.qcontext['product_id'] = product_id
+            response.qcontext['product_name'] = product_name
+            response.qcontext['product_price'] = product_price 
         
         return response
     
